be1fe48d09a25975fb07b4010847237a42b8b737
Byaxy <charlesbyaxy@gmail.com>
2023-07-18 15:01:32 +0000

    code cleanup in settings

--- app/settings/products-categories/[id]/subcategory/[subID]/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CategoryDetailsComponent from "@/components/CategoryDetailsComponent";
-
-export default function SubCategoryPage() {
-  return <CategoryDetailsComponent />;
-}
